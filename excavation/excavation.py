400feff087453945f89d14655ecdcd0d4b9d5c1a
eddymj96 <eddymj96@googlemail.com>
2022-05-16 15:17:49 +0100

    Fixed a few bugs in excavation.py

--- excavation/excavation.py
+++ excavation/excavation.py
@@ -23,6 +23,7 @@ from bosdyn.api import geometry_pb2, image_pb2, manipulation_api_pb2
 from bosdyn.client.image import ImageClient
 from bosdyn.client.manipulation_api_client import ManipulationApiClient
 from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
+from bosdyn.client.estop import EstopEndpoint, EstopKeepAlive
 
 g_image_click = None
 g_image_display = None
@@ -57,9 +58,9 @@ def get_image(image_client):
     else:
         img = cv2.imdecode(img, -1)
 
-    return img
+    return image, img
 
-def walk_to_object(config):
+def walk_to_object(robot, image_client):
     robot.logger.info("Powering on robot... This may take a several seconds.")
     robot.power_on(timeout_sec=20)
     assert robot.is_powered_on(), "Robot power on failed."
@@ -76,7 +77,7 @@ def walk_to_object(config):
 
     # TODO: Orient gripper color camera to view scene
 
-    img = get_image(image_client)
+    image, img = get_image(image_client)
     blurred = cv2.GaussianBlur(img, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
@@ -94,33 +95,14 @@ def walk_to_object(config):
             print('"q" pressed, exiting.')
             exit(0)
 
-    # Show the image to the user and wait for them to click on a pixel
-    robot.logger.info('Click on an object to walk up to...')
-    image_title = 'Click to walk up to something'
-    cv2.namedWindow(image_title)
-    cv2.setMouseCallback(image_title, cv_mouse_callback)
-
-    global g_image_click, g_image_display
-    g_image_display = img
-    cv2.imshow(image_title, g_image_display)
-    while g_image_click is None:
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q') or key == ord('Q'):
-            # Quit
-            print('"q" pressed, exiting.')
-            exit(0)
-
     robot.logger.info('Walking to object at image location (' + str(g_image_click[0]) +
                       ', ' + str(g_image_click[1]) + ')')
 
     walk_vec = geometry_pb2.Vec2(x=g_image_click[0], y=g_image_click[1])
 
     # Optionally populate the offset distance parameter.
-    if config.distance is None:
-        offset_distance = None
-    else:
-        offset_distance = wrappers_pb2.FloatValue(value=config.distance)
-
+    offset_distance = None
+    
     # Build the proto
     walk_to = manipulation_api_pb2.WalkToObjectInImage(
         pixel_xy=walk_vec, transforms_snapshot_for_camera=image.shot.transforms_snapshot,
@@ -162,21 +144,21 @@ def walk_to_object(config):
 
 
 def cv_mouse_callback(event, x, y, flags, param):
-global g_image_click, g_image_display
-clone = g_image_display.copy()
-if event == cv2.EVENT_LBUTTONUP:
-    g_image_click = (x, y)
-else:
-    # Draw some lines on the image.
-    #print('mouse', x, y)
-    color = (30, 30, 30)
-    thickness = 2
-    image_title = 'Click to walk up to something'
-    height = clone.shape[0]
-    width = clone.shape[1]
-    cv2.line(clone, (0, y), (width, y), color, thickness)
-    cv2.line(clone, (x, 0), (x, height), color, thickness)
-    cv2.imshow(image_title, clone)
+    global g_image_click, g_image_display
+    clone = g_image_display.copy()
+    if event == cv2.EVENT_LBUTTONUP:
+        g_image_click = (x, y)
+    else:
+        # Draw some lines on the image.
+        #print('mouse', x, y)
+        color = (30, 30, 30)
+        thickness = 2
+        image_title = 'Click to walk up to something'
+        height = clone.shape[0]
+        width = clone.shape[1]
+        cv2.line(clone, (0, y), (width, y), color, thickness)
+        cv2.line(clone, (x, 0), (x, height), color, thickness)
+        cv2.imshow(image_title, clone)
 
 
 def arg_float(x):
@@ -189,8 +171,6 @@ def arg_float(x):
 
 def main(argv):
     options = Options("192.168.80.3", "Kuno", "olympickuno1")
-
-    bosdyn.client.util.setup_logging(config.verbose)
 
     sdk = bosdyn.client.create_standard_sdk('WalkToObjectClient')
     robot = sdk.create_robot(options.hostname)
@@ -211,7 +191,7 @@ def main(argv):
 
     with bosdyn.client.lease.LeaseKeepAlive(image_client, must_acquire=True, return_at_exit=True):
         try:
-            execute_excavation(leamanipulation_api_client)
+            execute_excavation(robot, manipulation_api_client)
             return True
         except Exception as exc:  # pylint: disable=broad-except
             logger = bosdyn.client.util.get_logger()
